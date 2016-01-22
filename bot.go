a80cd5c0dad54d9e826b1b941384e0d1d7f59f39
Andrew Mason <andrew@fixedpoint.xyz>
2016-01-21 19:40:37 -0500

    add a helpful link. requires more manually control over the api calls

--- bot.go
+++ bot.go
@@ -119,7 +119,31 @@ func (bot *Bot) SetRealNameFields(message map[string]interface{}) interface{} {
 	if channel != bot.Channels["general"] {
 		return nil
 	}
-	return bot.DirectMessage(message["user"].(string), "Please set your real name fields")
+	userID := message["user"].(string)
+	dmChan := make(chan string)
+	userChan := make(chan interface{})
+	go func() {
+		dm, _ := bot.OpenDirectMessage(userID)
+		dmChan <- dm
+	}()
+	go func() {
+		resp, err := bot.Call("users.info", url.Values{"user": []string{userID}})
+		payload, err := httpToJSON(resp, err)
+		userChan <- payload
+	}()
+	payload := (<- userChan).(map[string]interface{})
+	success := payload["ok"].(bool)
+	if !success {
+		fmt.Println(payload)
+		return nil
+	}
+	user := payload["user"].(map[string]interface{})
+	nick := user["name"].(string)
+	text := "Please set your real name fields. https://hacsoc.slack.com/team/%s."
+	text += " Then click \"Edit\"."
+	text = fmt.Sprintf(text, nick)
+	dm := <- dmChan
+	return Message(text, dm)
 }
 
 func (bot *Bot) DirectMessage(user, text string) interface{} {
