2c492418e6780343e20899f8e8fc2de72200bb41
Andrew Mason <andrew@fixedpoint.xyz>
2016-01-21 19:10:47 -0500

    bot now PMs when a user joins general

--- bot.go
+++ bot.go
@@ -114,10 +114,33 @@ func (bot *Bot) ConstructReply(message map[string]interface{}, subtype interface
 
 func (bot *Bot) SetRealNameFields(message map[string]interface{}) interface{} {
 	channel := message["channel"].(string)
-	if channel != bot.Channels["slackers"] { // currently testing with slackers (this should be general)
+	if channel != bot.Channels["general"] {
 		return nil
 	}
-	return Mention(message["user"].(string), channel, "", "Please set your real name fields")
+	return bot.DirectMessage(message["user"].(string), "Please set your real name fields")
+}
+
+func (bot *Bot) DirectMessage(user, text string) interface{} {
+	dm, err := bot.OpenDirectMessage(user)
+	if err != nil {
+		return nil
+	}
+	return Message(text, dm)
+}
+
+func (bot *Bot) OpenDirectMessage(user string) (string, error) {
+	resp, err := bot.Call("im.open", url.Values{"user": []string{user}})
+	payload, err := httpToJSON(resp, err)
+	if err != nil {
+		return "", err
+	}
+	success := payload["ok"].(bool)
+	if !success {
+		fmt.Println(payload)
+		return "", nil // need an actual error here
+	}
+	channel := payload["channel"].(map[string]interface{})
+	return channel["id"].(string), nil
 }
 
 func Mention(nick, channel, beforeNick, afterNick string) interface{} {
