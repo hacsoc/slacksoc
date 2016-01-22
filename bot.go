a7314185a21545346641194d93157fc274188748
Andrew Mason <andrew@fixedpoint.xyz>
2016-01-21 20:05:28 -0500

    move some things around. these huge files are making me crazy

--- bot.go
+++ bot.go
@@ -146,49 +146,6 @@ func (bot *Bot) SetRealNameFields(message map[string]interface{}) interface{} {
 	return Message(text, dm)
 }
 
-func (bot *Bot) DirectMessage(user, text string) interface{} {
-	dm, err := bot.OpenDirectMessage(user)
-	if err != nil {
-		return nil
-	}
-	return Message(text, dm)
-}
-
-func (bot *Bot) OpenDirectMessage(user string) (string, error) {
-	resp, err := bot.Call("im.open", url.Values{"user": []string{user}})
-	payload, err := httpToJSON(resp, err)
-	if err != nil {
-		return "", err
-	}
-	success := payload["ok"].(bool)
-	if !success {
-		fmt.Println(payload)
-		return "", nil // need an actual error here
-	}
-	channel := payload["channel"].(map[string]interface{})
-	return channel["id"].(string), nil
-}
-
-func Mention(nick, channel, beforeNick, afterNick string) interface{} {
-	text := beforeNick
-	nick = "<@" + nick + ">"
-	text += nick
-	if text == nick {
-		text += ": "
-	}
-	text += afterNick
-	return Message(text, channel)
-}
-
-func Message(text, channel string) interface{} {
-	return map[string]string{
-		"id": time.Now().Format("010206150405"),
-		"type": "message",
-		"channel": channel,
-		"text": text,
-	}
-}
-
 func main() {
 	token := os.Getenv(TOKEN_VAR)
 	if token == "" {
---
+++ message.go
@@ -0,0 +1,19 @@
+func Mention(nick, channel, beforeNick, afterNick string) interface{} {
+	text := beforeNick
+	nick = "<@" + nick + ">"
+	text += nick
+	if text == nick {
+		text += ": "
+	}
+	text += afterNick
+	return Message(text, channel)
+}
+
+func Message(text, channel string) interface{} {
+	return map[string]string{
+		"id": time.Now().Format("010206150405"),
+		"type": "message",
+		"channel": channel,
+		"text": text,
+	}
+}
